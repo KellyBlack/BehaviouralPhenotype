dba321a13d513831d73ad46235caf98378e98b95
KellyBlack <kjblack@gmail.com>
2019-11-08 19:54:16 -0500

    main.cpp now set up to approximation the solution to a linear, second order ODE.

--- pde/approx/butterflyVWaspPDE/main.cpp
+++ pde/approx/butterflyVWaspPDE/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <fstream>
 #include <cmath>
 
 #include "util.h"
 #include "legendre.h"
+#include "lu_decomp.h"
 
+#define OUTPUTFILE "approximation.csv"
 
 int main()
 {
@@ -26,19 +29,18 @@ int main()
     double *gaussWeights  = ArrayUtils<double>::onetensor(N+1);
     double *gaussAbscissa = ArrayUtils<double>::onetensor(N+1);
 
+    // Define the matrices and vectors used to solve the linear systems.
+    double **jacobian = ArrayUtils<double>::twotensor(N+1,N+1);
+    double *baseFunc  = ArrayUtils<double>::onetensor(N+1);
+    double *deltaX  = ArrayUtils<double>::onetensor(N+1);
+    int    *order     = ArrayUtils<int>::onetensor(N+1);
+
     // Miscellaneous helper variables, for loops for example.
     int innerLupe;
     int outerLupe;
 
     // Define the Gauss quadrature.
     Legendre<double>::leg_quad(gaussAbscissa,gaussWeights,N);
-    std::cout << "Abscissa: " << std::endl;
-    for (innerLupe=0;innerLupe<=N;++innerLupe)
-        std::cout << innerLupe << ": " << gaussAbscissa[innerLupe] << std::endl;
-
-    std::cout << "Weights: " << std::endl;
-    for (outerLupe=0;outerLupe<=N;++outerLupe)
-        std::cout << outerLupe << ": " << gaussWeights[outerLupe] << std::endl;
 
     // Define the stiffness and mass matrices for the Legendre collocation method.
     std::cout << "Pre-processing" << std::endl;
@@ -46,24 +48,33 @@ int main()
     Legendre<double>::leg_der(D1,lval,gaussAbscissa,N,N);
     Legendre<double>::stiffLeg(stiff,gaussWeights,D1,N);
 
-    // Test the D1 matrix
-    std::cout << "Testing" << std::endl;
-    //double x2 = 0.0;
-    double sum;
-#define POWER 20
+    for(outerLupe=0;outerLupe<N;++outerLupe)
+        std::cout << gaussAbscissa[outerLupe] << ",";
+    std::cout << gaussAbscissa[N] << std::endl;
+
+    // Build an approximation to an ODE
     for(outerLupe=0;outerLupe<=N;++outerLupe)
-    {
-        sum = 0.0;
         for(innerLupe=0;innerLupe<=N;++innerLupe)
         {
-            sum += D1[outerLupe][innerLupe]*pow(gaussAbscissa[innerLupe],static_cast<double>(POWER));
-                   //gaussWeights[innerLupe]*pow(gaussAbscissa[innerLupe],static_cast<double>(outerLupe));
+            jacobian[outerLupe][innerLupe] = stiff[outerLupe][innerLupe];
         }
-        std::cout << "Deriv: " << sum << " - " << static_cast<double>(POWER)*pow(gaussAbscissa[outerLupe],static_cast<double>(POWER-1)) << std::endl;
-        //x2 += 1.0;
-        //std::cout << "integral: " << sum << " - " << (pow(1.0,static_cast<double>(outerLupe+1))-pow(-1.0,static_cast<double>(outerLupe+1)))/x2 << std::endl;
+    for(outerLupe=0;outerLupe<=N;++outerLupe)
+    {
+        jacobian[outerLupe][outerLupe] -= gaussWeights[outerLupe];
+        baseFunc[outerLupe] = 1.0*gaussWeights[outerLupe];
     }
 
+    if(!LU_Decomposition<double>::lu_decomp(jacobian,order,N+1))
+    {
+        std::cout << "The matrix is singular" << std::endl;
+    }
+    else
+    {
+        LU_Decomposition<double>::solve_lu(jacobian,deltaX,baseFunc,order,N+1);
+        for(outerLupe=0;outerLupe<N;++outerLupe)
+            std::cout << deltaX[outerLupe] << ",";
+         std::cout << deltaX[N] << std::endl;
+    }
 
     // Delete the arrays that have been allocated.
     ArrayUtils<double>::deltwotensor(lhsArray);
@@ -75,6 +86,12 @@ int main()
     // Delete the vectors that have been allocated.
     ArrayUtils<double>::delonetensor(gaussWeights);
     ArrayUtils<double>::delonetensor(gaussAbscissa);
+
+    // Delete the arrays and vectors used for the linear systems.
+    ArrayUtils<double>::deltwotensor(jacobian);
+    ArrayUtils<double>::delonetensor(baseFunc);
+    ArrayUtils<double>::delonetensor(deltaX);
+    ArrayUtils<int>::delonetensor(order);
 
     std::cout << "Done" << std::endl;
     return(0);
