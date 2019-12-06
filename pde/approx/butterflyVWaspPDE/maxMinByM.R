78298873dd250794520f965cd79230cf878c52d0
KellyBlack <kjblack@gmail.com>
2019-12-06 18:08:07 -0500

    Made the new R script more general to make different graphs for different m's in the same file

--- pde/approx/butterflyVWaspPDE/maxMinByM.R
+++ pde/approx/butterflyVWaspPDE/maxMinByM.R
@@ -1,28 +1,46 @@
-a <- read.csv('../build-butterflyVWaspPDE-Desktop-Debug/changingM-mu-0.2.csv')
-b <- read.csv('../build-butterflyVWaspPDE-Desktop-Debug/changingM-mu-0.02.csv')
-c <- read.csv('../build-butterflyVWaspPDE-Desktop-Debug/changingM-mu-0.1.csv')
+a <- read.csv('../build-butterflyVWaspPDE-Desktop-Debug/changingM-multipleMu.csv')
 
-highest <- max(c(a$maxButterfly,b$maxButterfly,c$maxButterfly))
+plot.new()
+plot.window(xlim=c(0,1.05*max(a$m)),
+            ylim=c(0,1.05*max(a$maxButterfly)))
+title(main="Max and Min Of The Butterfly Density After A Long Time Span",
+      xlab='m',
+      ylab='Butterfly Density')
+axis(1)
+axis(2)
+currentColour <- 0
+muLevels <- sort(unique(a$mu))
+colours <- c()
+labels <- c()
+for(mu in muLevels)
+{
+    currentColour <- currentColour + 1
+    labels <- c(labels,as.expression(bquote(mu == .(mu))))
+    ##labels <- c(labels,parse(text=paste0('mu',"=",mu)))
+    colours <- c(colours,currentColour)
+    points(a$m[a$mu==mu],a$maxButterfly[a$mu==mu],
+           type='l',col=currentColour,lwd=2)
+    points(a$m[a$mu==mu],a$minButterfly[a$mu==mu],
+           type='l',col=currentColour,lwd=2)
+}
+
+
 
 #pdf("/tmp/behaviourPhenotypeSteadyState.pdf")
-plot(b$m,b$minButterfly,type='l',
-     ylim=c(0,1.05*highest),
-     main="Max and Min Of The Butterfly Density After A Long Time Span",
-     xlab='m',
-     ylab='Butterfly Density',
-     col=2,lwd=2)
-points(b$m,b$maxButterfly,type='l',col=2,lwd=2)
-points(a$m,a$maxButterfly,type='l',col=4,lwd=2)
-points(a$m,a$minButterfly,type='l',col=4,lwd=2)
-points(c$m,c$maxButterfly,type='l',col=6,lwd=2)
-points(c$m,c$minButterfly,type='l',col=6,lwd=2)
+#plot(b$m,b$minButterfly,type='l',
+#     ylim=c(0,1.05*highest),
+#     main="Max and Min Of The Butterfly Density After A Long Time Span",
+#     xlab='m',
+#     ylab='Butterfly Density',
+#     col=2,lwd=2)
 
-legend(0.2,1.1,
-       c(expression(paste(mu,'=0.2')),
-         expression(paste(mu,'=0.1')),
-         expression(paste(mu,'=0.02'))),
-       lty=c(1,1,1),
-       col=c(4,6,2),
-       lwd=c(2,2,2)
-       )
+legend(0.1,1.15,labels,lty=1,col=colours,lwd=2)
+#legend(0.2,1.1,
+#       c(expression(paste(mu,'=0.2')),
+#         expression(paste(mu,'=0.1')),
+#         expression(paste(mu,'=0.02'))),
+#       lty=c(1,1,1),
+#       col=c(4,6,2),
+#       lwd=c(2,2,2)
+#       )
 #dev.off()
