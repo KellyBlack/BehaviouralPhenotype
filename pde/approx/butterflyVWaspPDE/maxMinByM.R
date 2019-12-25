b0982552b0d385c6afef6fd0f8a4194bec5fb86a
Kelly Black <kjblack@gmail.com>
2019-12-25 10:01:05 -0500

    moved the plotting commands in the max/min routine to a seperate function

--- pde/approx/butterflyVWaspPDE/maxMinByM.R
+++ pde/approx/butterflyVWaspPDE/maxMinByM.R
@@ -1,5 +1,39 @@
-#a <- read.csv('../build-butterflyVWaspPDE-Desktop-Debug/changingM-multipleMu.csv')
-a <- read.csv('../build-butterflyVWaspPDE-Desktop-Debug/changingMResults.csv')
+plotResults <- function(a)
+{
+    currentColour <- 0
+    muLevels  <- sort(unique(a$mu),decreasing=FALSE)
+    colours   <- c()
+    labels    <- c()
+    plotTypes <- c()
+    pchTypes  <- c()
+    nextPCH   <- 0
+    for(mu in muLevels)
+    {
+        nextPCH       <- nextPCH + 1
+        currentColour <- currentColour + 1
+        labels    <- c(labels,as.expression(bquote(mu == .(mu))))
+        ##labels  <- c(labels,parse(text=paste0('mu',"=",mu)))
+        colours   <- c(colours,currentColour)
+        plotTypes <- c(plotTypes,0)
+        pchTypes  <- c(pchTypes,nextPCH)
+
+        m <- a$m[a$mu==mu]
+        maxButterfly <- a$maxButterfly[a$mu==mu]
+        minButterfly <- a$minButterfly[a$mu==mu]
+        m <- sort(m,index.return=TRUE)
+        points(m$x,maxButterfly[m$ix],type='p',pch=nextPCH,col=currentColour,cex=0.75) # lwd=2,
+        points(m$x,minButterfly[m$ix],type='p',pch=nextPCH,col=currentColour,cex=0.75) # lwd=2
+    }
+
+    return(list(labels=labels,
+                plotTypes=plotTypes,
+                pchTypes=pchTypes,
+                colours=colours))
+    
+}
+
+##a <- read.csv('../build-butterflyVWaspPDE-Desktop-Debug/changingM-multipleMu.csv')
+a <- read.csv('../build-butterflyVWaspPDE-Desktop-Debug/changingMResults_1.csv')
 
 plot.new()
 plot.window(xlim=c(0,1.05*max(a$m)),
@@ -9,30 +43,7 @@ title(main="Max and Min Of The Butterfly Density After A Long Time Span",
       ylab='Butterfly Density')
 axis(1)
 axis(2)
-currentColour <- 0
-muLevels  <- sort(unique(a$mu),decreasing=FALSE)
-colours   <- c()
-labels    <- c()
-plotTypes <- c()
-pchTypes  <- c()
-nextPCH   <- 0
-for(mu in muLevels)
-{
-    nextPCH       <- nextPCH + 1
-    currentColour <- currentColour + 1
-    labels    <- c(labels,as.expression(bquote(mu == .(mu))))
-    ##labels  <- c(labels,parse(text=paste0('mu',"=",mu)))
-    colours   <- c(colours,currentColour)
-    plotTypes <- c(plotTypes,0)
-    pchTypes  <- c(pchTypes,nextPCH)
-
-    m <- a$m[a$mu==mu]
-    maxButterfly <- a$maxButterfly[a$mu==mu]
-    minButterfly <- a$minButterfly[a$mu==mu]
-    m <- sort(m,index.return=TRUE)
-    points(m$x,maxButterfly[m$ix],type='p',pch=nextPCH,col=currentColour,cex=0.75) # lwd=2,
-    points(m$x,minButterfly[m$ix],type='p',pch=nextPCH,col=currentColour,cex=0.75) # lwd=2
-}
+results <- plotResults(a)
 
 ## m <- a$m[a$mu==mu]
 ## tim <- a$time[a$mu==mu]
@@ -52,7 +63,8 @@ for(mu in muLevels)
 #     ylab='Butterfly Density',
 #     col=2,lwd=2)
 
-legend(0.1,0.35,labels,lty=plotTypes,pch=pchTypes,col=colours,lwd=2)
+legend(0.1,0.35,results$labels,lty=results$plotTypes,
+       pch=results$pchTypes,col=results$colours,lwd=2)
 #legend(0.2,1.1,
 #       c(expression(paste(mu,'=0.2')),
 #         expression(paste(mu,'=0.1')),
