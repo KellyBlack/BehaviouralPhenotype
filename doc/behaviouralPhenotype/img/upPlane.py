8d26ead7e29167e9a1cc9c18a75cad2656215c36
KellyBlack <kjblack@gmail.com>
2020-05-12 16:17:54 -0400

    Made final changes to the script that generates the stability region in the up plane. Set it up to save results as a pdf file

--- doc/behaviouralPhenotype/img/upPlane.py
+++ doc/behaviouralPhenotype/img/upPlane.py
@@ -24,8 +24,8 @@ def makeStabilityRegionPlot(c,m,largeU,largeP,p,axes):
     axes.fill_between(pShade,pShadeTop,pShadeBottom,facecolor="grey")
     axes.plot(p,p,'r',linewidth=2)
     axes.plot(p,(p-c)/2.0,'r',linewidth=2)
-    axes.plot(numpy.array([1.0,1.0]),numpy.array([0.0,1.0]),'k:')
-    axes.plot(numpy.array([1.0+m,1.0+m]),numpy.array([0.0,1.0+m]),'k:')
+    axes.plot(numpy.array([1.0,1.0]),numpy.array([0.0,1.0]),'k:',linewidth=2)
+    axes.plot(numpy.array([1.0+m,1.0+m]),numpy.array([0.0,1.0+m]),'k:',linewidth=2)
     
     axes.text(5.0,6.0,r"$u=p$")
     axes.text(6.0,1.0,r"$u=\frac{p-c}{2}$")
@@ -57,23 +57,25 @@ largeP = largeU
 p = numpy.array([0.0,largeP])
 
 
-fig = plt.figure()
+fig = plt.figure(figsize=(10,10))
 axes = fig.add_subplot(1, 1, 1)
 makeStabilityRegionPlot(c,m,largeU,largeP,p,axes)
-plt.show()
-#plt.close()
+#plt.show()
+plt.savefig("odeStability-uv-plane.pdf")
+plt.close()
 
-fig = plt.figure()
-plt.subplots_adjust(wspace=0.5)
+fig = plt.figure(figsize=(20,10))
+plt.subplots_adjust(wspace=0.15)
 axes = fig.add_subplot(1, 2, 1)
 makeStabilityRegionPlot(c,m*0.75,largeU,largeP,p,axes)
-axes.plot(numpy.array([1.0,1.0+m*0.75]),numpy.array([0.75,0.75]),'b--')
+axes.plot(numpy.array([1.0,1.0+m*0.75]),numpy.array([0.75,0.75]),'b--',linewidth=2)
 
 axes = fig.add_subplot(1, 2, 2)
 makeStabilityRegionPlot(c,m*1.2,largeU,largeP,p,axes)
-axes.plot(numpy.array([1.0,1.0+m*1.2]),numpy.array([0.75,0.75]),'b--')
+axes.plot(numpy.array([1.0,1.0+m*1.2]),numpy.array([0.75,0.75]),'b--',linewidth=2)
+plt.savefig("odeStability-uv-plane-Line.pdf")
 
-plt.show()
-
+#plt.show()
+plt.close()
 
 
