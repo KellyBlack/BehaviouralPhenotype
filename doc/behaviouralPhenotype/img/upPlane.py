43aeb10cf66ea3e38bf444bc2282c6577e3efd8a
KellyBlack <kjblack@gmail.com>
2020-05-19 14:52:41 -0400

    Changed the python script to generate the images in the up plane.

--- doc/behaviouralPhenotype/img/upPlane.py
+++ doc/behaviouralPhenotype/img/upPlane.py
@@ -4,17 +4,20 @@
 import numpy
 import matplotlib.pyplot as plt
 
-def makeStabilityRegionPlot(c,m,largeU,largeP,p,axes):
+def makeStabilityRegionPlot(c,m,largeU,largeP,p,axes,
+                            title="Stability Region For The ODEs"):
     
-    axes.set_xlabel(r"$p=1+m\theta$")
-    axes.set_ylabel(r"$u=\frac{cd}{g-d}$",labelpad=-20)
-    axes.set_title("Stability Region For The ODEs")
+    axes.set_xlabel(r"$p=1+m\theta$", fontsize=25)
+    axes.set_ylabel(r"$u=\frac{cd}{g-d}$",labelpad=-20, fontsize=25)
+    axes.set_title(title, fontsize=35)
     #axes.ticklabel_format(axis='y', style='sci', scilimits=(-1,1))
     
     axes.set_xticks(numpy.array([1.0,c,1.0+m])) 
     axes.set_xticklabels(["1","c","1+m"])
     axes.set_yticks(numpy.array([-c/2.0,0.0])) 
     axes.set_yticklabels([r"$-\frac{c}{2}$","0"])
+    axes.tick_params(axis='both', which='major', labelsize=25)
+    axes.tick_params(axis='both', which='minor', labelsize=30)
     
     
     pShade       = numpy.array([0.0,c,largeP])
@@ -27,8 +30,8 @@ def makeStabilityRegionPlot(c,m,largeU,largeP,p,axes):
     axes.plot(numpy.array([1.0,1.0]),numpy.array([0.0,1.0]),'k:',linewidth=2)
     axes.plot(numpy.array([1.0+m,1.0+m]),numpy.array([0.0,1.0+m]),'k:',linewidth=2)
     
-    axes.text(5.0,6.0,r"$u=p$")
-    axes.text(6.0,1.0,r"$u=\frac{p-c}{2}$")
+    axes.text(5.0,6.0,r"$u=p$", fontsize=25)
+    axes.text(6.0,1.0,r"$u=\frac{p-c}{2}$", fontsize=25)
     
     # Move left y-axis and bottim x-axis to centre, passing through (0,0)
     axes.spines['left'].set_position('zero')
@@ -59,7 +62,7 @@ p = numpy.array([0.0,largeP])
 
 fig = plt.figure(figsize=(10,10))
 axes = fig.add_subplot(1, 1, 1)
-makeStabilityRegionPlot(c,m,largeU,largeP,p,axes)
+makeStabilityRegionPlot(c,m,largeU,largeP,p,axes,"Stability Region For The ODEs")
 #plt.show()
 plt.savefig("odeStability-uv-plane.pdf")
 plt.close()
@@ -67,15 +70,16 @@ plt.close()
 fig = plt.figure(figsize=(20,10))
 plt.subplots_adjust(wspace=0.15)
 axes = fig.add_subplot(1, 2, 1)
-makeStabilityRegionPlot(c,m*0.75,largeU,largeP,p,axes)
+makeStabilityRegionPlot(c,m*0.75,largeU,largeP,p,axes,"")
 axes.plot(numpy.array([1.0,1.0+m*0.75]),numpy.array([0.75,0.75]),'b--',linewidth=2)
 
 axes = fig.add_subplot(1, 2, 2)
-makeStabilityRegionPlot(c,m*1.2,largeU,largeP,p,axes)
+makeStabilityRegionPlot(c,m*1.2,largeU,largeP,p,axes,"")
 axes.plot(numpy.array([1.0,1.0+m*1.2]),numpy.array([0.75,0.75]),'b--',linewidth=2)
-plt.savefig("odeStability-uv-plane-Line.pdf")
+fig.suptitle("Stability Region For The ODEs",fontsize=30)
 
 #plt.show()
+plt.savefig("odeStability-uv-plane-Line.pdf")
 plt.close()
 
 
