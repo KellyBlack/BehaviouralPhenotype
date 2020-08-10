28e7991dfd5af72473093f7fa3b1b311c290fc92
KellyBlack <kjblack@gmail.com>
2020-08-10 18:46:16 -0400

    Changed the ode max min graphing file to use different colours on the top and bottom of the plot

--- pde/approx/butterflyVWaspPDE/odeMaxMinByM.R
+++ pde/approx/butterflyVWaspPDE/odeMaxMinByM.R
@@ -1,90 +1,23 @@
-plotResults <- function(a)
-{
-    currentColour <- 0
-    muLevels  <- sort(unique(a$mu),decreasing=FALSE)
-    colours   <- c()
-    labels    <- c()
-    plotTypes <- c()
-    pchTypes  <- c()
-    nextPCH   <- 0
-    for(mu in muLevels)
-    {
-        nextPCH       <- nextPCH + 1
-        currentColour <- currentColour + 1
-        labels    <- c(labels,as.expression(bquote(mu == .(mu))))
-        ##labels  <- c(labels,parse(text=paste0('mu',"=",mu)))
-        colours   <- c(colours,currentColour)
-        plotTypes <- c(plotTypes,0)
-        pchTypes  <- c(pchTypes,nextPCH)
-
-        m <- a$m[a$mu==mu]
-        maxButterfly <- a$maxButterfly[a$mu==mu]
-        minButterfly <- a$minButterfly[a$mu==mu]
-        m <- sort(m,index.return=TRUE)
-        points(m$x,maxButterfly[m$ix],type='p',pch=nextPCH,col=currentColour,cex=0.75) # lwd=2,
-        points(m$x,minButterfly[m$ix],type='p',pch=nextPCH,col=currentColour,cex=0.75) # lwd=2
-    }
-
-    return(list(labels=labels,
-                plotTypes=plotTypes,
-                pchTypes=pchTypes,
-                colours=colours))
-    
-}
-
-filename <- '../build-butterflyVWaspPDE-Desktop-Debug/changingM-multipleMu.csv'
-filename <- '../build-butterflyVWaspPDE-Desktop-Debug/changingMResults_1.csv'
-filename <- '../build-butterflyVWaspPDE-Desktop-Debug/changingMResults_c=1.1.csv'
-#filename <- '../build-butterflyVWaspPDE-Desktop-Debug/changingMHysteresisReverse.csv'
-a <- read.csv(filename)
+ode <- read.csv('../build-butterflyVWaspPDE-Desktop-Debug/rk45_c1.1.csv')
+#ode <- read.csv('../build-butterflyVWaspPDE-Desktop-Debug/rk45.csv')
+odeOrder <- sort(ode$m,index.return=TRUE)
 
 plot.new()
-plot.window(xlim=c(0,1.05*max(a$m)),
-            ylim=c(0,1.16*max(a$maxButterfly)))
+plot.window(xlim=c(0,1.05*max(ode$m)),
+            ylim=c(0,1.16*max(ode$maxButterfly)))
 title(main="Max and Min Of The Butterfly Density After A Long Time Span",
       xlab='m',
       ylab='Butterfly Density')
 axis(1)
 axis(2)
-results <- plotResults(a)
 
-## m <- a$m[a$mu==mu]
-## tim <- a$time[a$mu==mu]
-## points(m,tim,pch=3,col=3)
-
-
-#plot(a$m[a$mu==0.02],a$time[a$mu==0.02])
-#points(a$m[a$mu==0.04],a$time[a$mu==0.04],col=2)
+##pdf("/tmp/behaviourPhenotypeSteadyState.pdf")
+closeValues <- abs(ode$maxButterfly[odeOrder$ix]-ode$minButterfly[odeOrder$ix])< 3.71e-4
+points(ode$m[odeOrder$ix],ode$minButterfly[odeOrder$ix],type='l',lty=1,lwd=3,col=4)
+points(ode$m[odeOrder$ix],ode$maxButterfly[odeOrder$ix],type='l',lty=1,lwd=3,col=2)
+points(ode$m[closeValues],ode$minButterfly[closeValues],type='l',lty=1,lwd=3,col=1)
 
 
-
-#pdf("/tmp/behaviourPhenotypeSteadyState.pdf")
-#plot(b$m,b$minButterfly,type='l',
-#     ylim=c(0,1.05*highest),
-#     main="Max and Min Of The Butterfly Density After A Long Time Span",
-#     xlab='m',
-#     ylab='Butterfly Density',
-#     col=2,lwd=2)
-
-ode <- read.csv('../build-butterflyVWaspPDE-Desktop-Debug/rk45_c1.1.csv')
-odeOrder <- sort(ode$m,index.return=TRUE)
-points(ode$m[odeOrder$ix],ode$minButterfly[odeOrder$ix],type='l',lty=3,lwd=4)
-points(ode$m[odeOrder$ix],ode$maxButterfly[odeOrder$ix],type='l',lty=3,lwd=4)
-results$labels <- c(results$labels,'ODE')
-results$plotTypes <- c(results$plotTypes,3)
-results$pchTypes <- c(results$pchTypes,-1)
-results$colours <- c(results$colours,1)
-
-legend(0.1,0.8,results$labels,lty=results$plotTypes,
-       pch=results$pchTypes,col=results$colours,lwd=2)
-#legend(0.2,1.1,
-#       c(expression(paste(mu,'=0.2')),
-#         expression(paste(mu,'=0.1')),
-#         expression(paste(mu,'=0.02'))),
-#       lty=c(1,1,1),
-#       col=c(4,6,2),
-#       lwd=c(2,2,2)
-#       )
 #dev.off()
 #dev.copy(pdf,'maxMinByM-mu-01-04.pdf')
 #dev.off()
